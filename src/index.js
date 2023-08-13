8f9b8d92709782c973f27a9c94af7778a3df0798
Arter Tendean <arter.tendean.07@gmail.com>
2023-08-14 02:25:38 +0800

    Update to the new API

--- src/index.js
+++ src/index.js
@@ -1,65 +1,49 @@
-const axios = require("axios");
-const validator = require("validator");
-const FormData = require("form-data");
+import axios from 'axios';
+import FormData from 'form-data';
+import fs from 'fs';
 
-const fs = require("fs");
+const uploadFromURL = (filename, url) => {
+  const data = new FormData();
+  data.append('filename', filename);
+  data.append('url', url);
 
-const upload = (filename, file) => {
-  // check if file url is valid
-  const isURL = validator.isURL(file);
+  const config = {
+    method: 'post',
+    url: 'https://discord-storage.animemoe.us/api/upload-from-url/',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    },
+    data: data
+  };
 
-  if (isURL) {
-    // upload from url
-    const data = new FormData();
-    const url = file;
-    data.append("filename", filename);
-    data.append("url", url);
-
-    const config = {
-      method: "post",
-      url: "https://discord-storage.animemoe.us/upload-from-url/",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        Referer: "jscord-storage-0.0.10",
-        ...data.getHeaders(),
-      },
-      data: data,
-    };
-
-    return axios(config)
-      .then(function (response) {
-        return { status: response.status, data: response.data };
-      })
-      .catch(function (error) {
-        return { status: error.response.status, data: error.response.data };
-      });
-  } else {
-    // upload from file
-    const data = new FormData();
-    data.append("filename", filename);
-    data.append("file", fs.createReadStream(file));
-
-    const config = {
-      method: "post",
-      url: "https://discord-storage.animemoe.us/upload-from-file/",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        Referer: "jscord-storage-0.0.10",
-        ...data.getHeaders(),
-      },
-      data: data,
-    };
-
-    return axios(config)
-      .then(function (response) {
-        return { status: response.status, data: response.data };
-      })
-      .catch(function (error) {
-        return { status: error.response.status, data: error.response.data };
-      });
-  }
+  return axios(config)
+    .then(response => ({ success: true, data: response.data }))
+    .catch(() => ({ success: false, data: {} }));
 };
 
-exports.upload = upload;
+const uploadFromFile = filePath => {
+  // return immediately if the file is not existing
+  if (!fs.existsSync(filePath)) {
+    return { success: false, data: {} };
+  }
+
+  const data = new FormData();
+  data.append('file', fs.createReadStream(filePath));
+
+  const config = {
+    method: 'post',
+    url: 'https://discord-storage.animemoe.us/api/upload-from-file/',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    },
+    data: data
+  };
+
+  return axios(config)
+    .then(response => ({ success: true, data: response.data }))
+    .catch(() => ({ success: false, data: {} }));
+};
+
+export { uploadFromFile, uploadFromURL };
